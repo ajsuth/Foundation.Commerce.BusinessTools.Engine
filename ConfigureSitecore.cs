df54fdcf869f9df5d83d7c8ea4db9e7fbca12f08
Andrew Sutherland <andrew.sutherland@sitecore.com>
2018-08-05 19:47:33 +1000

    Created plugin for referencing custom icons.

---
+++ ConfigureSitecore.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConfigureSitecore.cs" company="Sitecore Corporation">
+//   Copyright (c) Sitecore Corporation 1999-2017
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Foundation.Commerce.BusinessTools.Engine
+{
+	using System.Reflection;
+	using Microsoft.Extensions.DependencyInjection;
+	using Sitecore.Commerce.Core;
+	using Sitecore.Commerce.Plugin.BusinessUsers;
+	using Sitecore.Framework.Configuration;
+	using Sitecore.Framework.Pipelines.Definitions.Extensions;
+	using Sitecore.Framework.Rules;
+
+	/// <summary>
+	/// The configure sitecore class.
+	/// </summary>
+	public class ConfigureSitecore : IConfigureSitecore
+	{
+		/// <summary>
+		/// The configure services.
+		/// </summary>
+		/// <param name="services">
+		/// The services.
+		/// </param>
+		public void ConfigureServices(IServiceCollection services)
+		{
+			var assembly = Assembly.GetExecutingAssembly();
+			services.RegisterAllPipelineBlocks(assembly);
+			services.Sitecore().Rules(config => config.Registry(registry => registry.RegisterAssembly(assembly)));
+
+			services.Sitecore().Pipelines(config => config
+				.ConfigurePipeline<IBizFxNavigationPipeline>(pipeline => pipeline
+					.Add<Pipelines.Blocks.GetIconsNavigationViewBlock>().After<GetNavigationViewBlock>()
+				)
+			);
+
+			services.RegisterAllCommands(assembly);
+		}
+	}
+}
---
+++ Pipelines/Blocks/EntityViews/GetIconsNavigationViewBlock.cs
@@ -0,0 +1,34 @@
+﻿using Sitecore.Commerce.Core;
+using Sitecore.Commerce.EntityViews;
+using Sitecore.Framework.Conditions;
+using Sitecore.Framework.Pipelines;
+using System.Threading.Tasks;
+
+namespace Foundation.Commerce.BusinessTools.Engine.Pipelines.Blocks
+{
+	public class GetIconsNavigationViewBlock : PipelineBlock<EntityView, EntityView, CommercePipelineExecutionContext>
+	{
+		public override Task<EntityView> Run(EntityView entityView, CommercePipelineExecutionContext context)
+		{
+			Condition.Requires(entityView).IsNotNull($"{Name}: The argument cannot be null.");
+
+			foreach (var icon in Constants.Icons)
+			{
+				var iconDashboardView = new EntityView()
+				{
+					Name = icon,
+					ItemId = icon,
+					Icon = icon,
+					DisplayRank = 10
+				};
+				entityView.ChildViews.Add(iconDashboardView);
+			}
+
+			return Task.FromResult(entityView);
+		}
+
+		public GetIconsNavigationViewBlock() : base(null)
+		{
+		}
+	}
+}
